6e69f0b6d1d8aee863c419d9c25bb0b83929b747
融合应用事业部-王玉珀 <1106774262@qq.com>
2024-03-28 10:29:23 +0800

    [^]更新内容

--- src/views/v3s/1-what/index.vue
+++ src/views/v3s/1-what/index.vue
@@ -4,7 +4,9 @@
     <p>为了大家在阅读完本书籍后，能直接使用 vue3 做项目。从本节开始，大家就需要了解 v3s 。</p>
     <p class="mt-3">放心，这里只会简单的介绍 v3s 的使用方法，而你也只需要这些，就完全能够独立完成 vue3 的项目。</p>
     <p class="mt-3">是的 v3s 就是这么优秀，它足以让你立刻进入开发。</p>
-    <img src="/v3s.jpg" class="mt-3 w-[200px]" />
+    <a href="https://github.com/wangyupo/v3s" target="_blank" title="跳转至 v3s github地址" class="inline-block">
+      <img src="/v3s.jpg" class="mt-3 w-[200px] cursor-pointer" />
+    </a>
   </div>
 </template>
 
